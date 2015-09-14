7c92f2233e8969fb8e89aefea69af1f1c5d9f9c7
Jarod Stirling <jarodstirling@Alveyworld-04.local>
2015-09-14 10:20:31 -0600

    todays practices

---
+++ grade scaler.py
@@ -0,0 +1,14 @@
+# program to figure out grades
+
+score = int (raw_input("Enter your test score"))
+
+if score >= 93:
+	print "A"
+elif score >= 90:
+	print "A-"
+elif score >= 87:
+	print "B+"
+elif score >= 83:
+	print "B"
+elif score >= 80:
+	print "B-"
